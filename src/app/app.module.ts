7ca0dda4ac8b422ba572e8dc9a62aabccc2bad39
netng <net.nandang@gmail.com>
2022-10-12 12:26:03 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ButtonModule } from 'primeng/button';
+import {DividerModule} from 'primeng/divider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
